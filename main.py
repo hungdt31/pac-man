d01c141b52e5b4e3f338f74b9a7df46b8b22d15f
Tri Hung <koikoidth12@gmail.com>
2024-02-14 23:51:15 +0700

    finish day 1

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 import math
+from player_info import direction, counter, player_X , player_Y , player_images
 from board import boards
 pygame.init()
 
@@ -12,23 +13,32 @@ font = pygame.font.Font('freesansbold.ttf',20)
 level = boards
 color = 'blue'
 PI = math.pi
+flicker = False
 
+for i in range(1,5):
+  player_images.append(pygame.transform.scale(pygame.image.load(f'assets/player/{i}.bmp'),(40, 40)))
 def draw_board():
+  # chia screen thành các ô vuông nhỏ có width = num1 và height = num2
   num1 = ((HEIGHT - 50) // 32)
   num2 = (WIDTH // 30)
+
   for i in range(len(level)):
     for j in range(len(level[i])):
       if level[i][j] == 1:
+        # tham số thứ 3 được xem như tọa độ của tâm hình tròn
         pygame.draw.circle(screen, 'white', (j * num2 + (0.5*num2), i * num1 + (0.5 * num1)), 4)
-      if level[i][j] == 2:
+      if level[i][j] == 2 and not flicker:
         pygame.draw.circle(screen, 'white', (j * num2 + (0.5*num2), i * num1 + (0.5 * num1)), 10)
       if level[i][j] == 3:
-        pygame.draw.line(screen, color, (j * num2 + (0.5*num2), i * num1),
-                                        (j * num2 + (0.5*num2), i * num1 + num1), 3)
+        # vẽ đường thẳng : tham số thứ 3 và 4 lần lượt là tọa độ của điểm bắt đầu và kết thúc
+        # tham số cuối cùng là độ dày của đường thẳng
+        pygame.draw.line(screen, color, (j * num2 + (0.5 * num2), i * num1),
+                                        (j * num2 + (0.5 * num2), i * num1 + num1), 3)
       if level[i][j] == 4:
         pygame.draw.line(screen, color, (j * num2, i * num1 + (0.5 * num1)),
                                         (j * num2 + num2, i * num1 + (0.5 * num1)), 3)
       if level[i][j] == 5:
+        # tham số thứ 3 là hình chữ nhật chứa arc có gồm tọa độ góc trái trên và góc phải dưới
         pygame.draw.arc(screen, color, [(j * num2 - (num2 * 0.5)), (i * num1 + (0.5 * num1)), num2, num1], 0, PI / 2, 3)
       if level[i][j] == 6:
         pygame.draw.arc(screen, color, [(j * num2 + (num2 * 0.5)), (i * num1 + (0.5 * num1)), num2, num1], PI / 2, PI, 3)
@@ -40,14 +50,42 @@ def draw_board():
       if level[i][j] == 9:
         pygame.draw.line(screen, 'white', (j * num2, i * num1 + (0.5 * num1)),
                                           (j * num2 + num2, i * num1 + (0.5 * num1)), 3)
+def draw_player():
+  # 0 - RIGHT, 1 - LEFT, 2 - UP, 3 - DOWN
+  if direction == 0:
+    screen.blit(player_images[counter // 5], (player_X, player_Y))
+  elif direction == 1:
+    screen.blit(pygame.transform.flip(player_images[counter // 5], True, False), (player_X, player_Y))
+  elif direction == 2:
+    screen.blit(pygame.transform.rotate(player_images[counter // 5], 90), (player_X, player_Y))
+  elif direction == 3:
+    screen.blit(pygame.transform.rotate(player_images[counter // 5], 270), (player_X, player_Y))
 run = True
+
 while run:
   timer.tick(fps)
   screen.fill('black')
+  if counter < 19:
+    counter += 1
+    if counter > 3:
+      flicker = False
+  else:
+    counter = 0
+    flicker = True
   draw_board()
+  draw_player()
   # setting up pygame
   for event in pygame.event.get():
     if event.type == pygame.QUIT:
       run = False
+    if event.type == pygame.KEYDOWN:
+      if event.key == pygame.K_RIGHT:
+        direction = 0
+      if event.key == pygame.K_LEFT:
+        direction = 1
+      if event.key == pygame.K_UP:
+        direction = 2
+      if event.key == pygame.K_DOWN:
+        direction = 3
   pygame.display.flip()
 pygame.quit()
---
+++ player_info.py
@@ -0,0 +1,5 @@
+direction = 0
+counter = 0
+player_X = 550
+player_Y = 620
+player_images = []
